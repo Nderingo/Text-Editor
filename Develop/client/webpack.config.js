82a547674500d9e1def6a11c760f290512d67027
Emmanuel Lyimo <esal29t@gmail.com>
2022-04-27 02:00:53 -0700

    updated the webpack.config.js

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,8 +3,22 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
+
+module.exports = () => {
+  return {
+    mode: "development",
+    // Entry point for files
+    entry: {
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
+    },
+    // Output for our bundles
+    output: {
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-plugins: [
   new HtmlWebpackPlugin({
     template: './index.html',
     title: 'JATE'
@@ -22,7 +36,7 @@ plugins: [
     // TODO: Create a manifest.json:
       fingerprints: false,
       inject: true,
-      name: 'My Progressive Web App',
+      name: 'My Progressive Text Editor',
       short_name: 'JATE',
       description: 'My awesome Text Editor!',
       background_color: '#ffffff',
@@ -48,25 +62,29 @@ plugins: [
 ],
 // TODO: Add CSS loaders and babel to webpack.
 
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+module: {
+  // CSS loaders
+  rules: [
+    {
+      test: /\.css$/i,
+      use: ["style-loader", "css-loader"],
     },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+    {
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      // We use babel-loader in order to use ES6.
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"],
+          plugins: [
+            "@babel/plugin-proposal-object-rest-spread",
+            "@babel/transform-runtime",
+          ],
+        },
+      },
     },
-    plugins: [
-      
-    ],
-
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
+  ],
+},
+};
 };
